43dfda6571ca810c5a2b1413b48ffb6244083071
Zsh <2359595117@qq.com>
2018-12-05 20:39:48 +0800

    将本地工程添加到本地仓库

---
+++ src/cn/itcast/demo01.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
